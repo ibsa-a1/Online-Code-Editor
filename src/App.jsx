608527af40cca3a874c6e92de1ed5c5b01059d62
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-17 01:11:03 +0300

    use `Box` instead of `Heading`

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Heading } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Heading minH="100vh" bg="#0f0a19" color="gray.400" px={6} py={8}>
+    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
       Hello
-    </Heading>
+    </Box>
   );
 }
 
