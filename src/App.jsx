424f09a906ba0fabcc331689bfff8cf86ace43e5
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-16 20:38:51 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-amber-300">Hello </h1>
+    </div>
+  );
+}
+
+export default App;
