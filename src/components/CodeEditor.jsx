5453caf32bb1cd545ae3f6facbe0441d07f4d1e9
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-17 01:31:18 +0300

    state added to the CodeEditor

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,14 +1,18 @@
 import { Box } from "@chakra-ui/react";
 import { Editor } from "@monaco-editor/react";
-import React from "react";
+import React, { useState } from "react";
 
 const CodeEditor = () => {
+  const [value, setValue] = useState("");
   return (
     <Box>
       <Editor
-        height="90vh"
+        height="75vh"
+        theme="vs-dark"
         defaultLanguage="javascript"
         defaultValue="// some comment"
+        value={value}
+        onChange={(value) => setValue(value)}
       />
       ;
     </Box>
