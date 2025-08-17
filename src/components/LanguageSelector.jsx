5c55d233a9f97e8f10cfa8b6cdb53a474eba1ec6
ibsa-a1 <ibsaabera712@gmail.com>
2025-08-17 04:17:23 +0300

    Language Selector added and `MenuButton` Issue

---
+++ src/components/LanguageSelector.jsx
@@ -0,0 +1,37 @@
+import {
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
+import { LANGUAGE_VERSIONS } from "../contstants.js";
+
+const languages = Object.entries(LANGUAGE_VERSIONS);
+
+const LanguageSelector = ({ language, onSelect }) => {
+  return (
+    <Box ml={2} mb={4}>
+      <Text mb={2} fontSize="lg">
+        Language:
+      </Text>
+      <Menu>
+        <MenuButton as={Button}>JavaScript</MenuButton>
+        <MenuList>
+          {languages.map(([lang, version]) => (
+            <MenuItem key={language}>
+              {language}
+              &nbsp;
+              <Text as="span" color="gray.600" fontSize="sm">
+                ({version})
+              </Text>
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    </Box>
+  );
+};
+export default LanguageSelector;
